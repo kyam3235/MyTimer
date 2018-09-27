5462dcb026f54086ea619704bfea5f4a9073f230
Kyohei Yamaguchi <kyohei3235@gmail.com>
2018-09-27 15:43:15 +0900

    My first commit

---
+++ MyTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTimer
+//
+//  Created by Kyohei Yamaguchi on 2018/09/27.
+//  Copyright © 2018年 Kyohei Yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
