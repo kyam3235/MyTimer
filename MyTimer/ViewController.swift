57cddfe8387e1d41c61ccdeef0c864d2eed3b27d
Kyohei Yamaguchi <kyohei3235@gmail.com>
2018-10-02 21:13:14 +0900

    アプリ起動時のタイマー初期値を設定

--- MyTimer/ViewController.swift
+++ MyTimer/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //タイマーの変数を作成
+    var timer : Timer?
+    //カウント(経過時間)の変数を作成
+    var count = 0
+    //設定値を扱うキーを設定
+    let settingKey = "timer_value"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //UserDefaultsのインスタンスを作成
+        let settings = UserDefaults.standard
+        //UserDefaultsに初期値を登録
+        settings.register(defaults: [settingKey:10])
     }
 
 
